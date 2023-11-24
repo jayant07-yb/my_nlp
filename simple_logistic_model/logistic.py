5f9bd64274fd8b06669f4f463719e1525f9e9a15
Jayant Anand <jayantanand2001@gmail.com>
2023-11-24 19:41:03 +0530

    Added basic structure for finding learning rate

---
+++ simple_logistic_model/logistic.py
@@ -0,0 +1,15 @@
+"""
+Idea here is to test the performance of the different methods of iterating and finding the perfect learning rate.
+Datas:
+x --> integer
+y --> integer
+
+Let's try predicting the value of "y = (x-2000)^2 + 1000"
+For range of x = 0 to 1000
+
+Questions to be asked here:
+q.1 What is the best way to iterate over the data?
+q.2 Can dataset be divided into batches, with different weights for each batch?
+q.3 What is the best way to find the weights?
+q.4 What is the best way to find the best learning rate?
+"""
